ac8739434a6f2e784953974c094954252886b7f7
Tyler Myracle <tyler.myracle@gmail.com>
2023-10-25 18:27:49 -0500

    add logo to nav

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -1,6 +1,8 @@
 import { OrganizationSwitcher, SignedIn } from "@clerk/nextjs";
 import { MainNav } from "./main-nav";
 import { UserNav } from "./user-nav";
+import Image from "next/image";
+import Link from "next/link";
 
 export function Navbar() {
   return (
@@ -8,6 +10,17 @@ export function Navbar() {
       <div className="hidden flex-col md:flex">
         <div className="border-b">
           <div className="flex h-16 items-center px-4">
+            <Link href="/">
+              <div className="mr-6 flex items-center hover:cursor-pointer hover:underline">
+                <Image
+                  src="/propertyroom.png"
+                  width={48}
+                  height={48}
+                  alt="company logo"
+                />
+                <div className="ml-2 text-lg font-semibold">PropertyVault</div>
+              </div>
+            </Link>
             <SignedIn>
               <MainNav className="mx-6" />
             </SignedIn>
