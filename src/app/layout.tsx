3bf0440018e23dc7f550e595a907810aa32566c0
Tyler Myracle <tyler.myracle@gmail.com>
2024-01-08 16:51:18 -0600

    add spacer when logged in

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { TRPCReactProvider } from "~/trpc/react";
 import { Navbar } from "~/app/_components/nav/navbar";
 import { Toaster } from "~/app/_components/ui/toaster";
+import { SignedIn } from "@clerk/nextjs";
 
 export const fontSans = Inter({
   subsets: ["latin"],
@@ -37,6 +38,9 @@ export default function RootLayout({
           <ClerkProvider>
             <main className="min-h-screen w-full">
               <Navbar />
+              <SignedIn>
+                <div className="mt-14"></div>
+              </SignedIn>
               {children}
             </main>
             <Toaster />
