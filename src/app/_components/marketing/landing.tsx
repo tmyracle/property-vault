4ec847a99774afc1c9cf14687a95e5e3367e788a
Tyler Myracle <tyler.myracle@gmail.com>
2024-01-08 17:48:15 -0600

    fixing stuff

---
+++ src/app/_components/marketing/landing.tsx
@@ -0,0 +1,13 @@
+import Features from "~/app/_components/marketing/features";
+import Footer from "~/app/_components/marketing/footer";
+import Hero from "~/app/_components/marketing/hero";
+
+export default function Landing() {
+  return (
+    <div>
+      <Hero />
+      <Features />
+      <Footer />
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import Dashboard from "~/app/_components/dashboard/dashboard";
-import Landing from "~/app/_components/landing/landing";
+import Landing from "~/app/_components/marketing/landing";
 
 export default function Home() {
   return (
