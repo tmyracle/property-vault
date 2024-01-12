a921c3269b75e379fbcd4f7eef61c0116ea95027
Tyler Myracle <tyler.myracle@gmail.com>
2024-01-11 22:12:51 -0600

    add email for disbursement review

--- src/app/_components/email/request-reviewed.tsx
+++ src/app/_components/email/request-reviewed.tsx
@@ -16,21 +16,21 @@ import {
 } from "@react-email/components";
 import { env } from "~/env.mjs";
 
-interface NewDisbursementRequestTemplateProps {
+interface RequestReviewedTemplateProps {
   slug: string;
+  status: string;
+  caseNumber: string;
 }
-
-const previewText = `New disbursement request`;
 
 const logoUrl =
   "https://propertyvault.s3.us-east-2.amazonaws.com/public/propertyroom-crop.png";
 
-export const NewDisbursementRequestTemplate: React.FC<
-  Readonly<NewDisbursementRequestTemplateProps>
-> = ({ slug }) => (
+export const RequestReviewedTemplate: React.FC<
+  Readonly<RequestReviewedTemplateProps>
+> = ({ slug, status, caseNumber }) => (
   <Html>
     <Head />
-    <Preview>{previewText}</Preview>
+    <Preview>{`Your disbursement request was ${status} for case ${caseNumber}`}</Preview>
     <Tailwind>
       <Body className="mx-auto my-auto bg-white font-sans">
         <Container className="mx-auto my-[40px] w-[465px] rounded border border-solid border-[#eaeaea] p-[20px]">
@@ -44,12 +44,12 @@ export const NewDisbursementRequestTemplate: React.FC<
             />
           </Section>
           <Heading className="mx-0 my-[30px] p-0 text-center text-[24px] font-normal text-black">
-            New disbursement request ready for review
+            Your disbursement request was {status} for case {caseNumber}
           </Heading>
           <Text className="text-[14px] leading-[24px] text-black">Hello,</Text>
           <Text className="text-[14px] leading-[24px] text-black">
-            You have a new disbursement request ready for review on
-            PropertyVault. Please review the request using the button below.
+            Your disbursement request was {status} for case {caseNumber}. You
+            can access the finalized request from by clicking the button below.
           </Text>
           <Section className="mb-[32px] mt-[32px] text-center">
             <Button
@@ -71,8 +71,8 @@ export const NewDisbursementRequestTemplate: React.FC<
           </Text>
           <Hr className="mx-0 my-[26px] w-full border border-solid border-[#eaeaea]" />
           <Text className="text-[12px] leading-[24px] text-[#666666]">
-            You received this email because you are listed as a supervisor in
-            PropertyVault. If you were not expecting this request, you can
+            You received this email because you initiated a disbursement request
+            in PropertyVault. If you were not expecting this update, you can
             ignore this email. If you are concerned about your account&apos;s
             safety, please reply to this email to get in touch with us.
           </Text>
